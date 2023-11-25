a8cb4e4f385d0b28fde1b360d8bd75dfe5cfbf7a
Marco Muser <marco.muser@enpal.de>
2023-11-25 10:37:44 +0100

    Disable ImplicitUsings

--- WeatherForecast/WeatherForecast.cs
+++ WeatherForecast/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace dotnet_urlshortener;
 
 public class WeatherForecast
--- WeatherForecast/WeatherForecastController.cs
+++ WeatherForecast/WeatherForecastController.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace dotnet_urlshortener.Controllers;
 
